10dce23855423d03e27128f42634add4be0de7b4
Benny <benjamin.confiac@gmail.com>
2025-02-13 13:38:52 +0900

    vercel deploy

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 // const express = require('express'); this syntax is type "commonJS"
-import path from 'path';
-import express from 'express'; // this syntax is type "module" in package.json
-import dotenv from 'dotenv';
+import path from "path";
+import express from "express"; // this syntax is type "module" in package.json
+import dotenv from "dotenv";
 dotenv.config();
-import cookieParser from 'cookie-parser';
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 const port = process.env.PORT;
 
@@ -20,33 +21,41 @@ const app = express(); // initialize express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  cors({
+    origin: "https://react-shop-eight-dusky.vercel.app/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
 // cookie parser middleware
 app.use(cookieParser());
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.get('/api/config/paypal', (req, res) =>
-    res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
 const __dirname = path.resolve(); // set __dirname to current directory
-app.use('uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-    // any route that is not api will be redirected to index.html
-    app.get('*', (req, res) => 
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
-    app.get('/', (req, res) => {
-        res.send('API is running...');
-    });
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
 }
 
 app.use(notFound);
